c456a06c026935c6779ba94ac03c480bc4a57588
ThanhVy205 <lehuuthanhvy205@outlook.com>
2025-10-17 18:15:32 +0700

    clone về máy

---
+++ OnlineShoeStore/OnlineShoeStore/Controllers/Admin/DanhMucSanPhamfaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoeStore.Controllers.Admin
+{
+    public class DanhMucSanPhamfaultController : Controller
+    {
+        // GET: DanhMucSanPhamfault
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
